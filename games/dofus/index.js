d049a88cbfc500c52e72a82b6d848df4a76cabe3
Firzus <lprieu@gaming.tech>
2022-05-23 00:18:21 +0200

    add content

--- games/dofus/index.js
+++ games/dofus/index.js
@@ -4,14 +4,169 @@ const c = canvas.getContext('2d')
 canvas.width = 1024
 canvas.height = 576
 
-c.fillStyle = 'white'
-c.fillRect(0, 0, canvas.width, canvas.height)
+const collisionsMap = []
+for (let i = 0; i < collisions.length; i += 70) {
+    collisionsMap.push(collisions.slice(i, 70 + i))
+}
 
+class Boundary {
+    static width = 48
+    static height = 48
+    constructor({ position }) {
+        this.position = position
+        this.width = 48
+        this.height = 48
+    }
+
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+const boundaries = []
+const offset = {
+    x: -740,
+    y: -650
+}
+
+collisionsMap.forEach((row, i) => {
+    row.forEach((symbol, j) => {
+        if (symbol === 1025)
+        boundaries.push(
+            new Boundary({
+                position: {
+                    x: j * Boundary.width + offset.x,
+                    y: i * Boundary.height + offset.y
+                }
+            })
+        )
+    })
+})
 
 const image = new Image()
 image.src = './img/Pellet Town.png'
-console.log(image)
 
-image.onload = () => {
-    c.drawImage(image, 0, 0)
+const playerImage = new Image()
+playerImage.src = './img/playerDown.png'
+
+class Sprite {
+    constructor({
+        position,
+        velocity,
+        image
+    }) {
+        this.position = position
+        this.image = image
+    }
+
+    draw() {
+        c.drawImage(this.image, this.position.x, this.position.y)
+    }
 }
+
+const background = new Sprite({
+    position: {
+        x: offset.x,
+        y: offset.y
+    },
+    image: image
+})
+
+const keys = {
+    s: {
+        pressed: false
+    },
+    z: {
+        pressed: false
+    },
+    q: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }
+}
+
+
+const testBoundary = new Boundary({
+    position: {
+        x: 400,
+        y: 400
+    }
+})
+
+const movables = [background, testBoundary]
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    background.draw()
+    // boundaries.forEach(boundary => {
+    //     boundary.draw()
+    // })
+    testBoundary.draw()
+    c.drawImage(
+        playerImage,
+        0,
+        0,
+        playerImage.width / 4,
+        playerImage.height,
+        canvas.width / 2 - playerImage.width / 4 / 2,
+        canvas.height / 2 - playerImage.height / 2,
+        playerImage.width / 4,
+        playerImage.height
+    )
+
+    if (keys.z.pressed && lastKey === 'z') {
+        movables.forEach(movable => {
+            movable.position.y += 2})
+    else if (keys.q.pressed && lastKey === 'q') {
+        background.position.x += 2
+    }
+    else if (keys.d.pressed && lastKey === 'd') {
+        background.position.x -= 2
+    }
+    else if (keys.s.pressed && lastKey === 's') {
+        background.position.y -= 2
+    }
+}
+animate()
+
+let lastKey = ''
+window.addEventListener('keydown', (e) => {
+    switch (e.key) {
+        case 's':
+            keys.s.pressed = true
+            lastKey = 's'
+            break
+        case 'z':
+            keys.z.pressed = true
+            lastKey = 'z'
+            break
+        case 'q':
+            keys.q.pressed = true
+            lastKey = 'q'
+            break
+        case 'd':
+            keys.d.pressed = true
+            lastKey = 'd'
+            break
+    }
+})
+
+window.addEventListener('keyup', (e) => {
+    switch (e.key) {
+        case 's':
+            keys.s.pressed = false
+            break
+        case 'z':
+            keys.z.pressed = false
+            break
+        case 'q':
+            keys.q.pressed = false
+            break
+        case 'd':
+            keys.d.pressed = false
+            break
+    }
+})
